c26e0b25d79823c70ee0294ed396dcfc651556b0
pedroaf0 <pedroaf1233@gmail.com>
2020-01-10 19:51:55 -0300

    1

---
+++ js/creative.js
@@ -0,0 +1,77 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 75
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Magnific popup calls
+  $('#portfolio').magnificPopup({
+    delegate: 'a',
+    type: 'iframe',
+    iframe: {
+        patterns: {
+            youtube: {
+                index: 'youtube.com/', 
+                id: function(url) {        
+                    var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                    if ( !m || !m[1] ) return null;
+                    return m[1];
+                },
+                src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+            }
+        }
+    }
+  });
+  $('#services').magnificPopup({
+    delegate: 'a',
+    type: 'iframe',
+    iframe: {
+      markup: '<div style="background-color: white;    height: 700px;">'+
+                '<div class="mfp-close"></div>'+
+                '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKSXNSubBqMoAekSYBhKC543rTtXuE6GEUyEXGlEThB7x1fQ/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>'+
+              '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+    }    
+  });
+
+  function a(){
+    console.log("aaaaa");
+  }
+  $('.appsMaterialWizButtonPaperbuttonContent exportButtonContent').click(a);
+
+})(jQuery); // End of use strict
+
